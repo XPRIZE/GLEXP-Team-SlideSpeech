502e1b75f1bf674d04ce0106f9880cb52676469e
John Graves <john.graves@slidespeech.com>
2015-10-23 21:03:52 +1300

    Added logging via ajax calls to lettersketch_logging Flask API time/event/letter

---
+++ lettersketch/lettersketch_logging.py
@@ -0,0 +1,26 @@
+from flask import Flask
+app = Flask(__name__)
+import time
+import os
+
+LOG_FILE = 'lettersketch.log'
+print("Logging to",LOG_FILE)
+
+@app.route('/<event>/<letter>')
+def record_event(event,letter):
+    if os.path.exists(LOG_FILE):
+        with open(LOG_FILE,'a') as f:
+            f.write('{},{},{}\n'.format(time.time(),
+                event,
+                letter))
+    else:
+        with open(LOG_FILE,'w') as f:
+            f.write('time_utc,event,letter\n')
+            f.write('{},{},{}\n'.format(time.time(),
+                event,
+                letter))
+
+    return 'Hello World!'
+
+if __name__ == '__main__':
+    app.run()
